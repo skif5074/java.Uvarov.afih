6a54a94a5e17a1145700c3634b71383fb6f66cbf
Aleksandr <skif5074@outlook.com>
2023-03-02 18:17:56 +0300

    new progect

--- src/main/java/ru/netology/PurchaseItem.java
+++ src/main/java/ru/netology/PurchaseItem.java
@@ -2,9 +2,7 @@ package ru.netology;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class PurchaseItem {
